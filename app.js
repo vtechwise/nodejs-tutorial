2d572001b6d658fe487fb5217789af0e863fcc5e
victor <oguntayovictor65@gmail.com>
2025-01-29 23:26:59 +0100

    fs module sychronous approach

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-const { log } = require('console')
-const path = require('path')
-console.log(path.sep);
+const { readFileSync, writeFileSync } = require("fs");
 
-const filePath = path.join('/content/', 'subfolder', 'text.txt')
-console.log(filePath);
+const firstText = readFileSync("./content/subfolder/text.txt", "utf8");
 
+console.log(firstText);
 
-const base = path.basename(filePath)
-console.log(base);
-
-const absolute = path.resolve(__dirname, 'content', 'subfolder', '/text.txt')
-console.log(absolute);
-
-
+writeFileSync(
+  "./content/result-syc.txt",
+  `hello herer is the result ${firstText}`
+);
