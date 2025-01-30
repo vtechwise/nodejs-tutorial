0216fb6a25fe8c7aa82c5d29b37670c57d562f5b
victor <oguntayovictor65@gmail.com>
2025-01-31 00:10:35 +0100

    async await pattern

---
+++ 10. async-await.js
@@ -0,0 +1,19 @@
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/first.txt", "utf8");
+    const second = await readFilePromise("./content/second.txt", "utf8");
+    await writeFilePromise(
+      "./content/result-min-grenade.txt",
+      `Here is the result ${first} and ${second}`
+    );
+    console.log(first, second);
+  } catch (error) {}
+};
+
+start();
